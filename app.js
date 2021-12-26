4fbac03e82b8629a9a6b2ce5afbe3b2ab9762bb3
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-26 18:30:48 +0900

    theme toggling finished
    call to action added and finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
+
 const body = document.body;
 
 
