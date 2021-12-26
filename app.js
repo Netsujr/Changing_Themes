c95a7aa30c1ed52ac201ef9b2eaf44fafdd76a09
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-26 17:36:07 +0900

    refactored

--- app.js
+++ app.js
@@ -3,27 +3,50 @@ const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
-darkButton.onClick = () => {
-  // body.classList.remove('light');
-  // body.classList.add('dark');
+
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+}
+
+
+darkButton.onclick = () => {
   body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
 };
 
-lightButton.onClick = () => {
-  // body.classList.remove('dark');
-  // body.classList.add('light');
+lightButton.onclick = () => {
   body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
 };
 
-solarButton.onClick = () => {
+solarButton.onclick = () => {
 
-  if  (body.classList.contains('solar')) {
+  if (body.classList.contains('solar')) {
 
     body.classList.remove('solar');
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
+
+    solarButton.innerText = 'solarize';
+
+    localStorage.removeItem('isSolar');
 
   } else {
+
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `
+
     body.classList.add('solar');
+    solarButton.innerText = 'normalize';
+
+    localStorage.setItem('isSolar', true);
   }
-
-
 };
